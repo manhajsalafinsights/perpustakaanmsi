0b6a2a6fb6a8de0efce54bdaba910c86ff43e5dd
xroya24-pixel <xroya24@gmail.com>
2026-06-22 01:00:13 +0700

    Buku Berdasarkan Kategori: tambah 1 item

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -533,7 +533,7 @@ export default function HomeContent() {
                 <SectionSkeleton />
               ) : categoryPicks.length > 0 ? (
                 <div className={GRID_CLASSES}>
-                  {(expanded.kategori ? categoryPicks : categoryPicks.slice(0, defaultLimit)).map((book, i) => (
+                  {(expanded.kategori ? categoryPicks : categoryPicks.slice(0, defaultLimit + 1)).map((book, i) => (
                     <BookCard key={book.id} book={book} index={i} />
                   ))}
                 </div>
